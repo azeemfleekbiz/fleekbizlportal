0f8fcf9375882c63cd1ce51bead18b917b86b0bf
azeemfleekbiz <35449155+azeemfleekbiz@users.noreply.github.com>
2018-01-22 18:46:54 +0500

    bit changed in leftsidebar

--- resources/views/admin/layouts/leftsidebar.blade.php
+++ resources/views/admin/layouts/leftsidebar.blade.php
@@ -9,13 +9,13 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">        
         <li class="active treeview">
-          <a href="{{ url('/admin') }}">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>     
+          <a href="{{ url('/admin/dashboard') }}">
+            <i class="fa fa-dashboard" aria-hidden="true"></i> <span>Dashboard</span>        
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
-          </a>          
-        </li>
+          </a>
+        </li>          
        <li class="active treeview">
           <a href="">
             <i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Orders</span>
