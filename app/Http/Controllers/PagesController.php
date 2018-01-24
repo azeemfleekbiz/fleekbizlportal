ab51d9ba12f8907a7968acead5b9723b9a90f849
salmanfleekbiz <35482716+salmanfleekbiz@users.noreply.github.com>
2018-01-24 12:53:26 +0545

    Order form data processing complete

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,24 +2,124 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class PagesController extends Controller
 {
-
     public function homeVersion()
     {
     	$home = 'home';
     	return view('pages.'.$home);
     }
 
-    //Form for adding a new order in our database table.
-    public function create(Request $request){
-    	$fname = $request->input('fname');
+    public function createOrders(Request $request)
+    {
+      $fname = $request->input("fname");
+      $lname = $request->input("lname");
+      $email = $request->input("email");
+      $phone = $request->input("phone");
+      $logo_name = $request->input("logo_name");
+      $logo_category = $request->input("logo_category");
+      $target_audience = $request->input("target_audience");
+      $compititor_url = $request->input("compititor_url");
+      $slogan = $request->input("slogan");
+      $website_url = $request->input("website_url");
+      $descrp = $request->input("descrp");
+      $describe_imgs_like = $request->input("describe_imgs_like");
+      $describe_imgs_dont_like = $request->input("describe_imgs_dont_like");
+      $uploadfiles_name = $request->input("uploadfiles_name");
+      $remove_file_arr = explode(",",$uploadfiles_name);
+      $logo_type = $request->input("logo_type");
+      $logo_type_arr = implode(",",$logo_type);
+      $choose_color = $request->input("choose_color");
+      $other_color = $request->input("other_color");
+      $logo_usage = $request->input("logo_usage");
+      $other_logo_usage = $request->input("other_logo_usage");
+      $font_type = $request->input("font_type");
+      $other_font_type = $request->input("other_font_type");
+      $logo_feel = $request->input("logo_feel");
+      $logo_feel_arr = implode(",",$logo_feel);
+      $communicate_designers = $request->input("communicate_designers");
+      $deigner_help_files_name = $request->input("deigner_help_files_name");
+      $remove_deigner_help_files_arr = explode(",",$deigner_help_files_name);
 
-    	print_r($fname) ;
-    	die();
 
+      if ( Input::hasFile('sample_logos') ):
+            $file = Input::file();
+           for ($j=0; $j < count($file['sample_logos']); $j++) { 
+              $sample_images_name_arr[] = $file['sample_logos'][$j]->getClientOriginalName();
+            } 
+           if(count($remove_file_arr) > 0){
+              for ($i=0; $i < count($file['sample_logos']) ; $i++) {
+                   $sample_images_name = $file['sample_logos'][$i]->getClientOriginalName();
+                   if(in_array($sample_images_name_arr[$i], $remove_file_arr)){
+                      //echo 'Delete Files '.$sample_images_name.'<br>';
+                   }else{
+                      $destinationPath = $file['sample_logos'][$i];  
+                      $image_path ="public/uploads/order_logo/".$sample_images_name;  
+                      move_uploaded_file($destinationPath, $image_path); 
+                   }
+              }
+           }else{
+               for ($i=0; $i < count($file['sample_logos']) ; $i++) {
+                   $sample_images_name = $file['sample_logos'][$i]->getClientOriginalName();
+                   $destinationPath = $file['sample_logos'][$i];  
+                   $image_path ="public/uploads/order_logo/".$sample_images_name;  
+                   move_uploaded_file($destinationPath, $image_path); 
+               }
+           }
+      endif;
+
+      if ( Input::hasFile('deigner_help_imgs') ):
+            $help_file = Input::file();
+           for ($j=0; $j < count($help_file['deigner_help_imgs']); $j++) { 
+              $deigner_help_imgs_arr[] = $help_file['deigner_help_imgs'][$j]->getClientOriginalName();
+            } 
+           if(count($remove_deigner_help_files_arr) > 0){
+              for ($i=0; $i < count($help_file['deigner_help_imgs']) ; $i++) {
+                   $deigner_help_name = $help_file['deigner_help_imgs'][$i]->getClientOriginalName();
+                   if(in_array($deigner_help_imgs_arr[$i], $remove_deigner_help_files_arr)){
+                   }else{
+                      $destinationPath_help_imgs = $help_file['deigner_help_imgs'][$i];  
+                      $deigner_help_image_path ="public/uploads/order_logo/".$deigner_help_name;  
+                      move_uploaded_file($destinationPath_help_imgs, $deigner_help_image_path); 
+                   }
+              }
+           }else{
+               for ($i=0; $i < count($help_file['deigner_help_imgs']) ; $i++) {
+                   $deigner_help_name = $help_file['deigner_help_imgs'][$i]->getClientOriginalName();
+                   $destinationPath_help_imgs = $help_file['deigner_help_imgs'][$i];  
+                   $deigner_help_image_path ="public/uploads/order_logo/".$deigner_help_name;  
+                   move_uploaded_file($destinationPath_help_imgs, $deigner_help_image_path); 
+               }
+           }
+      endif;
+
+      echo 'Name F '.$fname.'<br>';
+      echo 'L Name '.$lname.'<br>';
+      echo 'Email '.$email.'<br>';
+      echo 'Phone '.$phone.'<br>';
+      echo 'Logo Name '.$logo_name.'<br>';
+      echo 'Logo Cate '.$logo_category.'<br>';
+      echo 'Target '.$target_audience.'<br>';
+      echo 'Compititor '.$compititor_url.'<br>';
+      echo 'web url '.$website_url.'<br>';
+      echo 'Descp '.$descrp.'<br>';
+      echo 'Sample Logo '.$image_path.'<br>';
+      echo 'Image Like '.$describe_imgs_like.'<br>';
+      echo 'Image dont like '.$describe_imgs_dont_like.'<br>';
+      echo 'Logo Type'.$logo_type_arr.'<br>';
+      echo 'Choose Color '.$choose_color.'<br>';
+      echo 'Other Color'.$other_color.'<br>';
+      echo 'Logo usage'.$logo_usage.'<br>';
+      echo 'Other usage'.$other_logo_usage.'<br>';
+      echo 'Font Type'.$font_type.'<br>';
+      echo 'Other Font'.$other_font_type.'<br>';
+      echo 'Logo Feel'.$logo_feel_arr.'<br>';
+      echo 'Communicate to our logo design team '.$communicate_designers.'<br>';
+       exit;
     }
 }
--- js/main.js
+++ js/main.js
@@ -41,4 +41,116 @@ isValid = true;
 prevStepWizard.removeAttr('disabled').trigger('click');
 });
 $('div.setup-panel div .navButton.first').trigger('click');
+
+
+$("#phone").mask("(999) 999-9999");
+
+
+//Upload Sample Logo Image
+var storedFiles = []; 
+var namess = $('#uploadfiles_name').val();
+var file_name_store = [];
+var sample_file_total = [];
+$('body').on('change', '.user_picked_files', function() {
+var files = this.files;
+var i = 0;
+for (i = 0; i < files.length; i++) {
+var readImg = new FileReader();
+var file = files[i];
+if (file.type.match('image.*')){
+storedFiles.push(file);
+readImg.onload = (function(file) {
+return function(e) {
+$('.cvf_uploaded_files').append(
+"<li file = '" + file.name + "'>" +                                
+"<img class = 'img-thumb' src = '" + e.target.result + "' height = '20px' width = '50px' />" +
+"<a href = '#' class = 'cvf_delete_image' title = 'Cancel'>Delete</a>" +
+"</li>"
+);     
+};
+})(file);
+readImg.readAsDataURL(file);
+} else {
+alert('the file '+ file.name + ' is not an image<br/>');
+}
+sample_file_total.push(file.name);
+document.getElementById('sample_file_total').value = sample_file_total.join();
+}
+});
+function remove(array, element) {
+    return array.filter(e => e !== element);
+}
+$('body').on('click','a.cvf_delete_image',function(e){
+e.preventDefault();
+$(this).parent().remove('');       
+var file = $(this).parent().attr('file');
+file_name_store.push(file);
+document.getElementById('uploadfiles_name').value = file_name_store.join();
+});   
+
+/*End Sample Logo Images*/
+
+//Document and Image Upload
+var storedFiles2 = []; 
+var namess2 = $('#uploadfiles_name').val();
+var file_name_store2 = [];
+var sample_file_total2 = [];
+$('body').on('change', '.user_help_files', function() {
+var files = this.files;
+var i = 0;
+for (i = 0; i < files.length; i++) {
+var readImg = new FileReader();
+var file = files[i];
+if (file.type.match('image.*')){
+storedFiles2.push(file);
+readImg.onload = (function(file) {
+return function(e) {
+$('.cvf_uploaded_help_files').append(
+"<li file = '" + file.name + "'>" +                                
+"<img class = 'img-thumb' src = '" + e.target.result + "' height = '20px' width = '50px' />" +
+"<a href = '#' class = 'cvf_delete_help_image' title = 'Cancel'>Delete</a>" +
+"</li>"
+);     
+};
+})(file);
+readImg.readAsDataURL(file);
+} else {
+alert('the file '+ file.name + ' is not an image<br/>');
+}
+sample_file_total2.push(file.name);
+document.getElementById('deigner_help_file_total').value = sample_file_total2.join();
+}
+});
+function remove(array, element) {
+    return array.filter(e => e !== element);
+}
+$('body').on('click','a.cvf_delete_help_image',function(e){
+e.preventDefault();
+$(this).parent().remove('');       
+var file = $(this).parent().attr('file');
+file_name_store2.push(file);
+document.getElementById('deigner_help_files_name').value = file_name_store2.join();
+});   
+
+/*End Document and Image*/
+
+//Select Package 
+$('body').on('click','a.selectpackage',function(e){
+e.preventDefault();
+var package_name = $(this).attr("name");
+var package_amount = $(this).attr("amount");
+document.getElementById('package_name').value = package_name;
+document.getElementById('package_amount').value = package_amount;
+}); 
+/* End Select Package */
+
+//Select Addon 
+$('body').on('click','a.add_adon',function(e){
+e.preventDefault();
+var addon_name = $(this).attr("title");
+var addon_amount = $(this).attr("amount");
+document.getElementById('addon_name').value = addon_name;
+document.getElementById('addon_amount').value = addon_amount;
+}); 
+/* End Addon */
 });
